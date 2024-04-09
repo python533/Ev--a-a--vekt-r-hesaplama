22aebfff95b7b7b5643acaa611fede9771bb2089
python533 <143389231+python533@users.noreply.github.com>
2024-04-09 12:25:54 +0300

    Calculate Vektör
    Soru=X evin boyu 484 cm, yanındaki ağacın boyu ise 223'cm dir. Buna göre evin boyu ağacın boyundan kaç cm fazladır?

---
+++ vektör hesapla.py
@@ -0,0 +1,10 @@
+class vektör:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    def evagac_vektör(self):
+        evagac_vektör=(self.x-self.y)
+        return evagac_vektör
+
+
